efbdd15fbcca36e35616db89141eb9fbd346390f
104466305 <104466305>
2026-08-14 19:39:55 +1000

    Pass Task

--- SampleApp104466305/Program.cs
+++ SampleApp104466305/Program.cs
@@ -10,7 +10,11 @@ namespace SampleApp104466305
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Deployment Activity 1: Pass Task Complete")
+            Console.WriteLine("Callum Gesthuizen 104466305");
+            Console.WriteLine("Deployment Activity 1: Pass Task Complete");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
         }
     }
 }
